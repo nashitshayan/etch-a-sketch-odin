4f59e2309b18a29efe705eda9e529696d425cc6d
nashitshayan <shayan.nashit@gmail.com>
2021-12-31 14:34:24 +0530

    added comments

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,7 +4,7 @@ const container= document.querySelector('.container');
 const btnReset = document.querySelector('.btn-reset');
 
 
-
+//the initial 16x16 grid
 
 for(let i=0; i<256; i++)
 {
@@ -18,21 +18,24 @@ for(let i=0; i<256; i++)
 
 let childDivs= Array.from(container.children);
 
-
+//when left-mouse is held down, apply mouseover event to all the child square divs
 const handleMouseDown = ()=>{
     Array.from(container.children).map(child=>{
         child.addEventListener('mousemove', handleMouseMove)
     })
 }
 
+//remove the event listeners on the children square divs when left-mouse is let go
 const handleMouseUp= ()=>{
     Array.from(container.children).map(child=>{
         child.removeEventListener('mousemove', handleMouseMove)
     })
 }
 
+//change bg color when the mouse moves over the sq divs
 const handleMouseMove = (e)=> e.target.style.backgroundColor='red'; //child
 
+//add event listeners on the parent div
 container.addEventListener('mousedown',handleMouseDown );
 container.addEventListener('mouseup', handleMouseUp);
 
